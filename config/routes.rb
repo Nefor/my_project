fb7b8188221a056eceb62973f9c9faf0ddebe1e0
Tatiana Davydiuk <tatianadavydiuk@gmail.com>
2017-08-15 18:06:17 +0300

    Configuration static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root 'apartments#index'
 
+
+  get '/signup', to: 'static_pages#signup'
+
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+
   resources :apartments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
